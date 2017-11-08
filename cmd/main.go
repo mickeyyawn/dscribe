d0509ee5a3c097619d6071da7265a3f99e388fd1
Mickey Yawn <mickeyyawn@gmail.com>
2017-11-08 16:23:35 -0500

    feature complete

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,16 @@ package main
 
 import (
 	"github.com/mickeyyawn/dscribe"
+	"fmt"
 )
 
 func main(){
 	dscribe.Init()
+	for i:=0;i<1000;i++ {
+		s := dscribe.Generate()
+
+		fmt.Print(s)
+		fmt.Println()
+	}
+	
 }
--- dscribe.go
+++ dscribe.go
@@ -1,9 +1,23 @@
 package dscribe
 
 import (
-    "fmt"
-    "io/ioutil"
+	"io/ioutil"
+	s "strings"
+	"math/rand"
+	"time"
 )
+
+
+var connectives []byte
+var lastNames []byte
+var words []byte
+
+var aConnectives []string
+var aLastNames []string
+var aWords []string
+
+var err error
+
 
 func check(e error) {
     if e != nil {
@@ -11,8 +25,32 @@ func check(e error) {
     }
 }
 
+func Generate() string {
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+	
+	i_lastNames := random.Intn(len(aLastNames))
+	i_words := random.Intn(len(aWords))
+	i_connectives := random.Intn(len(aConnectives))
+
+	theWord := string(aConnectives[i_connectives]) + "-" + string(aLastNames[i_lastNames]) + "-" + string(aWords[i_words])
+
+	return s.ToLower(theWord)
+
+}
+
 func Init() {
-	connective_words, err := ioutil.ReadFile("/usr/share/dict/connectives")
-    check(err)
-    fmt.Print(string(connective_words))
+
+	connectives, err = ioutil.ReadFile("connectives.txt")
+	check(err)
+	aConnectives = s.Split(string(connectives), "\n")
+
+	words, err = ioutil.ReadFile("words.txt")
+	check(err)
+	aWords = s.Split(string(words), "\n")
+
+	lastNames, err = ioutil.ReadFile("last-names.txt")
+	check(err)
+	aLastNames = s.Split(string(lastNames), "\n")
+
 }
